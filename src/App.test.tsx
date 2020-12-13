436441e19cea0dba1863acc7e06ec89816b5ff74
Edson Alcala <edson_alcala@msn.com>
2020-12-13 20:25:41 +0000

    added sample test

---
+++ src/App.test.tsx
@@ -0,0 +1,11 @@
+import { render } from '@testing-library/react'
+
+import App from './App'
+
+describe("App tests", () => {
+    test("snapshot", () => {
+        const { container } = render(<App />)
+
+        expect(container).toMatchSnapshot()
+    })
+})
