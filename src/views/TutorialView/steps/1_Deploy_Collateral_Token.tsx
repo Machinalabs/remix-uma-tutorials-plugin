9928b2b1b9ec61a16af5c16e24ba5c30b8865456
Edson Alcala <edson.alcala@consensys.net>
2020-12-21 17:46:11 +0000

    wip deploy collateral view

--- src/views/TutorialView/steps/1_Deploy_Collateral_Token.tsx
+++ src/views/TutorialView/steps/1_Deploy_Collateral_Token.tsx
@@ -3,7 +3,12 @@ import React from "react"
 export const DeployCollateralToken: React.FC = () => {
     return (
         <React.Fragment>
-            <h1>Deploy collateral token</h1>
+            <h4>Deploy collateral token</h4>
+            <p>This is the token that will serve as collateral for the synthethic token.</p>
+            <p>We will deploy it and give permission to the expiring multiparty creator to spend the collateral tokens on our behalf.</p>
+
+            {/* TODO: FORM to deploy a collateral token */}
+
         </React.Fragment>
     )
 }
