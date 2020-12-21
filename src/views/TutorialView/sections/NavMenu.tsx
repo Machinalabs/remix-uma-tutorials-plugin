0f97c9ab07fb0ded38c60ea28a3fd7b5fecffa6f
Edson Alcala <edson.alcala@consensys.net>
2020-12-21 17:46:23 +0000

    nav menu fixed styles

--- src/views/TutorialView/sections/NavMenu.tsx
+++ src/views/TutorialView/sections/NavMenu.tsx
@@ -1,6 +1,7 @@
 import React from "react"
 import { Nav } from "react-bootstrap"
 import { NavLink } from "react-router-dom"
+import styled from "styled-components"
 import { useStep } from "../hooks"
 
 export const NavMenu: React.FC = () => {
@@ -8,14 +9,41 @@ export const NavMenu: React.FC = () => {
 
     const allSteps = getAllSteps()
 
+    const handleOnNavClick = (e: any) => {
+        e.preventDefault();
+    }
+
     return (
         <Nav defaultActiveKey={allSteps["collateral_token"]} className="flex-column">
             {Object.keys(allSteps).map((item: string, index: number) => {
                 const text = `${index + 1}) ${allSteps[item]}`
-                return <NavLink key={index} to={`/tutorial/${item}`}><Nav.Link as="div" style={{ textDecoration: "none" }} key={item} href={item}>
-                    {text}
-                </Nav.Link></NavLink>
+                return <StyledNavLink onClick={handleOnNavClick} key={index} to={`/tutorial/${item}`} activeStyle={{
+                    opacity: 1,
+                    fontWeight: 500
+                }}>
+                    <Nav.Link as={StyledDiv} key={item} href={item} disabled={true}>
+                        {text}
+                    </Nav.Link>
+                </StyledNavLink>
             })}
         </Nav>
     )
 }
+
+
+const StyledNavLink = styled(NavLink)`
+    color: black;
+    opacity: 0.3;
+    &:hover {
+        text-decoration: none;
+        cursor: text;
+    }
+`
+
+const StyledDiv = styled.div`
+    color: black;
+
+    &:hover {
+        cursor: text;
+    }
+`
