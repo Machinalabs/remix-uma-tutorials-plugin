73feade60881b3e50de19bd5106003652fe6ac55
Edson Alcala <edson.alcala@consensys.net>
2020-12-24 12:19:14 +0000

    added deployment

--- src/utils/debug.ts
+++ src/utils/debug.ts
@@ -3,3 +3,7 @@ const LOG = process.env.REACT_APP_ENABLE_DEBUG || false
 export const log = (...args: any) => {
   if (LOG) console.log(args)
 }
+
+export const debug = (...args: any) => {
+  if (LOG) console.log(args)
+}
