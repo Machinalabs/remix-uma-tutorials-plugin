96d92a93a2817dbe92e96084ca583eef6d8960c8
Edson Alcala <edson.alcala@consensys.net>
2020-12-30 11:33:09 +0000

    added methods to track price identifiers, collateral and synts

--- src/views/TutorialView/hooks/useContract.test.tsx
+++ src/views/TutorialView/hooks/useContract.test.tsx
@@ -1,6 +1,7 @@
 import { renderHook, act } from "@testing-library/react-hooks"
+import { BigNumber } from "ethers"
 import { EthereumAddress, UMAContractName } from "../../../extras/deployment"
-import { useContract, ContractProvider } from "./useContract"
+import { useContract, ContractProvider, Token } from "./useContract"
 
 describe("useContract tests", () => {
   const render = () => {
@@ -11,7 +12,7 @@ describe("useContract tests", () => {
     }
   }
 
-  test("onCollateralDeployed", () => {
+  test("getContractAddress", () => {
     const { result } = render()
 
     const contracts = new Map<UMAContractName, EthereumAddress>()
@@ -26,4 +27,69 @@ describe("useContract tests", () => {
     expect(contractAddress).toBeDefined()
     expect(contractAddress).toEqual("0x00001")
   })
+
+  test("addPriceIdentifier", () => {
+    const { result } = render()
+
+    const newPriceIdentifier = "ETB/BTC"
+
+    expect(result.current.priceIdentifiers.length).toEqual(0)
+
+    act(() => {
+      result.current.addPriceIdentifier(newPriceIdentifier)
+    })
+
+    const priceIdentifiers = result.current.priceIdentifiers
+
+    expect(priceIdentifiers).toBeDefined()
+    expect(priceIdentifiers.length).toEqual(1)
+    expect(priceIdentifiers[0]).toEqual(newPriceIdentifier)
+  })
+
+  test("addCollateralToken", () => {
+    const { result } = render()
+
+    const newToken: Token = {
+      name: "WETH",
+      symbol: "WETH",
+      decimals: 18,
+      totalSupply: BigNumber.from("10000000")
+    }
+
+    expect(result.current.collateralTokens.length).toEqual(0)
+
+    act(() => {
+      result.current.addCollateralToken(newToken)
+    })
+
+    const collateralTokens = result.current.collateralTokens
+
+    expect(collateralTokens).toBeDefined()
+    expect(collateralTokens.length).toEqual(1)
+    expect(collateralTokens[0]).toEqual(newToken)
+  })
+
+  test("addSyntheticToken", () => {
+    const { result } = render()
+
+    const newToken: Token = {
+      name: "SNT",
+      symbol: "SNT",
+      decimals: 18,
+      totalSupply: BigNumber.from("10000000")
+    }
+
+    expect(result.current.syntheticTokens.length).toEqual(0)
+
+    act(() => {
+      result.current.addSyntheticToken(newToken)
+    })
+
+    const syntheticTokens = result.current.syntheticTokens
+
+    expect(syntheticTokens).toBeDefined()
+    expect(syntheticTokens.length).toEqual(1)
+    expect(syntheticTokens[0]).toEqual(newToken)
+  })
+
 })
