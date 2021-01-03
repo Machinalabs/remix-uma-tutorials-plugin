417d730a89cf22741eec2d148655230349875b70
Edson Alcala <edson.alcala@consensys.net>
2021-01-03 20:08:20 +0000

    added completed icons

--- src/views/TutorialView/sections/NavMenu.tsx
+++ src/views/TutorialView/sections/NavMenu.tsx
@@ -5,7 +5,7 @@ import styled from "styled-components"
 import { useStep, DEFAULT_STEP, StepDefinition } from "../hooks"
 
 export const NavMenu: React.FC = () => {
-  const { getAllSteps } = useStep()
+  const { getAllSteps, currentStep } = useStep()
 
   const allSteps = getAllSteps()
 
@@ -16,7 +16,7 @@ export const NavMenu: React.FC = () => {
   return (
     <Nav defaultActiveKey={allSteps[DEFAULT_STEP]} className="flex-column">
       {allSteps.map((currentStepDefinition: StepDefinition, index: number) => {
-        const text = `${index + 1}) ${currentStepDefinition.name}`
+        const text = `${currentStepDefinition.name}`
         return (
           <StyledNavLink
             onClick={handleOnNavClick}
@@ -25,9 +25,13 @@ export const NavMenu: React.FC = () => {
             activeStyle={{
               opacity: 1,
               fontWeight: 500,
+              fontSize: "0.9em"
             }}
           >
             <Nav.Link as={StyledDiv} key={currentStepDefinition.key} href={currentStepDefinition.route} disabled={true}>
+              <NumberContainer>
+                {currentStep.order > currentStepDefinition.order ? <CompletedIcon /> : <span>{currentStepDefinition.order}</span>}
+              </NumberContainer>
               {text}
             </Nav.Link>
           </StyledNavLink>
@@ -37,9 +41,29 @@ export const NavMenu: React.FC = () => {
   )
 }
 
+const CompletedIcon: React.FC = () => {
+  return (<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="white" className="bi bi-check2" viewBox="0 0 16 16">
+    <path d="M13.854 3.646a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L6.5 10.293l6.646-6.647a.5.5 0 0 1 .708 0z" />
+  </svg>)
+}
+
+const NumberContainer = styled.div`
+  display: inline-flex;
+  background-color: #007bff;
+  width: 24px;
+  height: 24px;
+  text-align: center;
+  border-radius: 50%;
+  margin-right: 8px;
+  color: white;
+  font-size: 0.8em;
+  align-items: center;
+  justify-content: center
+`
+
 const StyledNavLink = styled(NavLink)`
   color: black;
-  opacity: 0.3;
+  opacity: 0.5;
   &:hover {
     text-decoration: none;
     cursor: text;
